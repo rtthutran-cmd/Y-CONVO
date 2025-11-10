cdc61095157f0759ba77f3e5d308229d1c3dbf99
rtthutran-cmd <rt.thutran@gmail.com>
2025-11-09 19:17:21 -0500

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Y-Convo 💬",
+  description: "Your Conversation Coach",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
